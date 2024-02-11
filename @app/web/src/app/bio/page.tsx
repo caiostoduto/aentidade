217fa4a65459b81324c5e1c981d1070253f773e6
Caio Stoduto <caio.stoduto@outlook.com>
2024-02-11 01:06:12 -0300

    feat(web): create /app/bio

---
+++ @app/web/src/app/bio/page.tsx
@@ -0,0 +1,105 @@
+import Head from 'next/head';
+import Image from "next/image";
+import Link from 'next/link';
+
+export default function Bio() {
+  return (
+    <main className='flex flex-col items-center justify-center w-full px-[20px] py-16'>
+      <Head>
+        <title>Bio</title>
+      </Head>
+
+      <header className='grid place-items-center mb-4'>
+        <Image
+          className='relative pb-[20px]'
+          src="/logo_transparente.svg"
+          width={100}
+          height={100}
+          alt="aentidade logo"
+        />
+
+        <span className='mt-3.5 text-[19px]'>
+          aentidade
+        </span>
+
+        <div className='flex mt-2 mb-2'>
+          <Image
+            className='relative me-[2px]'
+            src="/icons/pin.svg"
+            height={14}
+            width={14}
+            alt="Ícone de pin"
+          />
+          <span className='text-[14px] opacity-80'>
+            Santo André, UFABC
+          </span>
+        </div>
+
+        <div className='flex'>
+          <Link href="https://instagram.com/aentidade.ufabc/" className='p-2'>
+            <Image
+              className='relative'
+              src="/icons/instagram.svg"
+              width={28}
+              height={28}
+              alt="Ícone do Instagram"
+            />
+          </Link>
+          <Link href="https://github.com/caiostoduto/aentidade" className='p-2'>
+            <Image
+              className='relative'
+              src="/icons/github.svg"
+              width={28}
+              height={28}
+              alt="Ícone do GitHub"
+            />
+          </Link>
+        </div>
+      </header>
+
+      <section className='mt-4 grid place-items-center max-w-[560px] w-full text-center'>
+        <a
+          href='https://aentidade.pages.dev/participe'
+          className='mb-4 border-2 p-[17.5px] rounded-2xl w-full bg-gray-800/20'>
+          <span className=''>
+            Faça parte!
+          </span>
+        </a>
+
+        <a
+          href='https://aentidade.pages.dev/calendário'
+          className='mb-4 border-2 p-[17.5px] rounded-2xl w-full bg-gray-800/20'>
+          <span>
+            Nosso Calendário
+          </span>
+        </a>
+
+        <a
+          href='https://aentidade.pages.dev/comentário'
+          className='mb-4 border-2 p-[17.5px] rounded-2xl w-full bg-gray-800/20'>
+          <span>
+            Denúncias, Elogios e Sugestões
+          </span>
+        </a>
+
+        <a
+          href='https://aentidade.pages.dev/normas'
+          className='mb-4 border-2 p-[17.5px] rounded-2xl w-full bg-gray-800/20'>
+          <span>
+            Normas aentidade
+          </span>
+        </a>
+      </section>
+
+      <section className='mt-6 max-w-[560px] w-full'>
+        <iframe
+          src='https://open.spotify.com/embed/playlist/4ONy8cyUkojEkYOWAHR0Fe?utm_source=generator&theme=0'
+          className='rounded-2xl w-full'
+          height={500}
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+        />
+      </section>
+    </main>
+  );
+}
--- @app/web/src/app/not-found.tsx
+++ @app/web/src/app/not-found.tsx
@@ -1,9 +1,10 @@
 'use client'
 
 import { usePathname } from 'next/navigation'
+import Head from 'next/head';
 import { useState, useEffect, type ReactElement } from 'react'
 import Image from 'next/image'
-import './styles/not-found.css'
+import '../styles/not-found.css'
 
 export default function NotFound(): ReactElement<any, any> | void {
   const code = (usePathname() as string)?.slice(1)
@@ -25,6 +26,10 @@ export default function NotFound(): ReactElement<any, any> | void {
 function LoadingScreen(): ReactElement<any, any> {
   return (
     <main className="flex flex-col items-center justify-center w-full h-screen">
+      <Head>
+        <title>Redirecting...</title>
+      </Head>
+
       <Image
         className='grow fixed'
         src="/logo_transparente.svg"
