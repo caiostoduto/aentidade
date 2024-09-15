2f25bf12c2373c452fa9606ab2b2b0cc6d0ddf96
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2024-09-15 01:51:43 -0300

    refactor(web): move part of tailwind style to css & loading size

--- @app/web/src/components/loading.tsx
+++ @app/web/src/components/loading.tsx
@@ -1,33 +1,26 @@
+import "../styles/loading.css";
+
 export default function LoadingAnimation() {
 	return (
-		<div
-			className="w-[30dvh] h-[31.2dvh] animate-spin"
-			style={{ animation: "spin 5s linear infinite" }}
-		>
-			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[70deg]">
-				<div
-					className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826]"
-					style={{ animation: "spin 2s linear infinite" }}
-				/>
+		<div className="loading-container animate-[spin_5s_linear_infinite]">
+			<div className="circle-container rotate-[70deg]">
+				<div className="circle primary-color animate-[spin_2s_linear_infinite]" />
 			</div>
 
-			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[140deg]">
-				<div
-					className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826]"
-					style={{ animation: "spin 2s linear infinite" }}
-				/>
+			<div className="circle-container rotate-[140deg]">
+				<div className="circle primary-color animate-[spin_5s_linear_infinite]" />
 			</div>
 
-			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826" />
+			<div className="circle-container rotate-[0deg]">
+				<div className="circle primary-color" />
 			</div>
 
-			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826" />
+			<div className="circle-container rotate-[0deg]">
+				<div className="circle primary-color" />
 			</div>
 
-			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[#ffffffb3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#ffffff26]" />
+			<div className="circle-container rotate-[0deg]">
+				<div className="circle secondary-color" />
 			</div>
 		</div>
 	);
