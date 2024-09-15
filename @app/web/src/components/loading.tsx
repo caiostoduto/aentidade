9cef038cadda24fb4c896993406293a9b73ae51d
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2024-09-15 01:59:55 -0300

    chore(web): rename .loading-container > loading-wrapper & .circle-container > .circle-wrapper

--- @app/web/src/components/loading.tsx
+++ @app/web/src/components/loading.tsx
@@ -1,25 +1,25 @@
-import "../styles/loading.css";
+import "@/styles/loading.css";
 
 export default function LoadingAnimation() {
 	return (
-		<div className="loading-container animate-[spin_5s_linear_infinite]">
-			<div className="circle-container rotate-[70deg]">
+		<div className="loading-wrapper animate-[spin_5s_linear_infinite]">
+			<div className="circle-wrapper rotate-[70deg]">
 				<div className="circle primary-color animate-[spin_2s_linear_infinite]" />
 			</div>
 
-			<div className="circle-container rotate-[140deg]">
+			<div className="circle-wrapper rotate-[140deg]">
 				<div className="circle primary-color animate-[spin_5s_linear_infinite]" />
 			</div>
 
-			<div className="circle-container rotate-[0deg]">
+			<div className="circle-wrapper rotate-[0deg]">
 				<div className="circle primary-color" />
 			</div>
 
-			<div className="circle-container rotate-[0deg]">
+			<div className="circle-wrapper rotate-[0deg]">
 				<div className="circle primary-color" />
 			</div>
 
-			<div className="circle-container rotate-[0deg]">
+			<div className="circle-wrapper rotate-[0deg]">
 				<div className="circle secondary-color" />
 			</div>
 		</div>
