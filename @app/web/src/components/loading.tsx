c7f2a034b6ea0231eeab2c0678fe71d51ec9a099
Caio Stoduto <88816833+caiostoduto@users.noreply.github.com>
2024-09-15 01:14:00 -0300

    fix(web): one of the circles not displaying

--- @app/web/src/components/loading.tsx
+++ @app/web/src/components/loading.tsx
@@ -6,28 +6,28 @@ export default function LoadingAnimation() {
 		>
 			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[70deg]">
 				<div
-					className="w-full h-full rounded-full border-[rgba(245,210,720.7)] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_rgba(245,210,72,0.15)]"
+					className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826]"
 					style={{ animation: "spin 2s linear infinite" }}
 				/>
 			</div>
 
 			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[140deg]">
 				<div
-					className="w-full h-full rounded-full border-[rgba(245,210,72,0.7)] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_rgba(245,210,72,0.15)]"
+					className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826]"
 					style={{ animation: "spin 2s linear infinite" }}
 				/>
 			</div>
 
 			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[rgba(245,210,72,0.7)] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_rgba(245,210,72,0.15)]" />
+				<div className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826" />
 			</div>
 
 			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[rgba(245,210,72,0.7)] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_rgba(245,210,72,0.15)]" />
+				<div className="w-full h-full rounded-full border-[#f5d248b3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#f5d24826" />
 			</div>
 
 			<div className="absolute w-[30dvh] h-[31.2dvh] rotate-[0deg]">
-				<div className="w-full h-full rounded-full border-[rgba(255,255,255,0.7)] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_rgba(255,255,255,0.15)]" />
+				<div className="w-full h-full rounded-full border-[#ffffffb3] border-solid border-r-0 border-t-0 border-l-[0.5dvh] border-b-[5px] shadow-[inset_0_0_10px_#ffffff26]" />
 			</div>
 		</div>
 	);
